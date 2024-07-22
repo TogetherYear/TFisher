07faa396944c0b726ff6c3590e6c0e8d0b17859f
TSingleton <togetheryear@gmail.com>
2024-07-22 16:27:36 +0800

    Update:init

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-//znvftf45u7m5ljfrwpnnneqycpvkxt4zhmlsin66njlklkeqf7yq
 
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('Fish.First', () => {
